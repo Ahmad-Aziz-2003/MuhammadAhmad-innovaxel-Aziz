5678e8845bcabff2f17137f7e9fa97606d4efb5f
Ahmad-Aziz-2003 <ahmadgcu321@gmail.com>
2025-07-16 21:28:29 +0500

    Initial project setup with backend and frontend folders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className=" bg-yellow-500 text-center p-6">
+      <h1 className="text-7xl font-bold text-blue-600">
+        Tailwind + Vite + React 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
